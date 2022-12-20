3b54fab7a8748c1684bbdd7f932a0edf5d137e8e
Hira-Gulzar <107863975+Hira-Gulzar@users.noreply.github.com>
2022-12-20 11:46:32 +0500

    Add files via upload

---
+++ ani.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main()
+{
+system("color 8F");
+cout << "      ------------------------------                                  " << endl;
+cout << "       O      ^____^                                              " << endl;
+cout << "              ( OO )\\_________                                     " << endl;
+cout << "              (____)\\         )\\/\\                              " << endl;
+cout << "                     ||----w  |                                 " << endl;
+cout << "                     ||      ||                                 " << endl;
+}
+                                                   
